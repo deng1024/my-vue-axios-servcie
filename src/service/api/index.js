241978f28b53f44382925285bbbdacce10e3b032
Dengbk <963021525@qq.com>
2020-01-13 11:10:14 +0800

    优化代码

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -6,14 +6,15 @@
 * 输出apis
 * */
 
-let apis = {};
+const modulesFiles = require.context('./modules', false, /\.js$/);  // 第二个argument为false，不处理子目录
 
-const files = require.context('./modules', false, /\.js$/); // 第二个argument为false，不处理子目录
-
-files.keys().forEach(filename => {
+const apis = modulesFiles.keys().reduce((modules, modulePath) => {
+  const value = modulesFiles(modulePath);
+  // set './app.js' => 'app' : modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
   // 取文件中export出来的moduleName作为模块名，如果没有moduleName则取文件名为模块名 ( login.js => login )
-  const moduleName = files(filename).moduleName || /\/(\w+)\.js$/.exec(filename)[1];
-  apis[moduleName] = files(filename).default;
-});
+  const moduleName = value.moduleName || modulePath.replace(/^\.\/(.*)\.\w+$/, '$1');
+  modules[moduleName] = value.default;
+  return modules;
+}, {});
 
 export default apis;
